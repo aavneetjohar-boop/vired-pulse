b66bffa308b8913188855fc2ae0873e94ccb07ee
aavneetjohar-boop <aavneet.johar@herovired.com>
2026-03-26 18:00:27 +0530

    Delete navigation.js

--- navigation.js
+++
@@ -1,9 +0,0 @@
-function showTab(tab) {
-
-  document.getElementById("learnTab").classList.add("hidden");
-  document.getElementById("callTab").classList.add("hidden");
-  document.getElementById("opsTab").classList.add("hidden");
-  document.getElementById("analyticsTabContent").classList.add("hidden");
-
-  document.getElementById(tab + "Tab").classList.remove("hidden");
-}
